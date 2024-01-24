51ac48837eef8b4cca41495df0512dac0948e855
Frendhi <frendhi.saido@outlook.com>
2024-01-24 18:14:44 +0700

    parameterized the tn-cli.py arg

--- tn-cli/node.js
+++ tn-cli/node.js
@@ -5,12 +5,13 @@ const app = express();
 app.use(express.json());
 const port = process.argv[2] || 3000;
 const ip = process.argv[3] || '0.0.0.0';
-
+const tinode_grpc_server = process.argv[4] || 'localhost:16060'; // 34.101.45.102:16060
 app.post('/execute', (req, res) => {
     console.log(req.body)
     // Request Body: {"script": "chatdemo --what cred --group grppQw179RgniM usrMSeBdwoBrJ8,usrMDDxrn4YuLg,usrHKiDc0XQudY\n"}
     const tinode_script = req.body.script;
-    let pythonProcess = spawn('python', ['tn-cli.py', '--host', '34.101.45.102:16060', '--verbose', '--login-basic','bob:bob123']);
+    // bob is ROOT. How? See tinode-db/README.md
+    let pythonProcess = spawn('python', ['tn-cli.py', '--host', tinode_grpc_server, '--verbose', '--login-basic','bob:bob123']);
 
     pythonProcess.stdin.write(tinode_script);
     pythonProcess.stdin.end();
